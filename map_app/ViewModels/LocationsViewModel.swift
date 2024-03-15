4cd38a182c2277ed9537bef67659300ec95a1b34
sherpa00 <nephack00@gmail.com>
2024-03-15 13:02:26 +0545

    feat(add): added map in views for map app

--- map_app/ViewModels/LocationsViewModel.swift
+++ map_app/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,46 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     @Published var locations: [Location]
     
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapPosition(location: mapLocation)
+        }
+    }
+    
+    @Published var mapPosition: MapCameraPosition
+    
+    private var mapSpan: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init() {
         let gotLocations: [Location] = LocationsDataService.locations
-        locations = gotLocations
+        self.locations = gotLocations
+        self.mapLocation = gotLocations.first!
+        
+        self.mapPosition = MapCameraPosition.region(
+            MKCoordinateRegion(
+                center: gotLocations.first!.coordinates,
+                span:  mapSpan
+            )
+        )
+        
+        self.updateMapPosition(location: gotLocations.first!)
+    }
+    
+    private func updateMapPosition(location: Location) {
+        withAnimation(.easeInOut) {
+            mapPosition = MapCameraPosition.region(
+                MKCoordinateRegion(
+                    center: location.coordinates,
+                    span: mapSpan
+                )
+            )
+        }
     }
 }
+
